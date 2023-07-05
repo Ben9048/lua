0dc36a94645e7afdc28d9b0766fae703778822cd
FunBot <61308126+Ben9048@users.noreply.github.com>
2023-07-06 04:32:01 +0500

    Add files via upload

--- opengate.lua
+++ opengate.lua
@@ -1,74 +1,68 @@
-script_name("FAST OPEN GATE")
+script_name("Model")
 script_author("Ben_Puls")
-script_description("fast open gate")
+script_description("Model")
 
-
-
-require "lib.moonloader"
-
+require 'lib.moonloader'
 local dlstatus = require('moonloader').download_status
-local keys = require "vkeys"
-local sampev = require 'lib.samp.events'
-local inicfg = require "inicfg"
+local inicfg = require 'inicfg'
+local keys = require 'vkeys'
+local imgui = require 'imgui'
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
 
 update_state = false
 
 local script_vers = 1
 local script_vers_text = "1.00"
-
-local update_url = "https://raw.githubusercontent.com/Ben9048/lua/main/update.ini?token=GHSAT0AAAAAACDSWBWIRKG5PFAUC7CSIWCGZFF4CRA"
-local update_path = getWorkingDirectory() .. "/update.ini" -- link
-
-local scirpt_url = "https://raw.githubusercontent.com/Ben9048/lua/main/opengate.lua" -- link
 local script_path = thisScript().path
+local script_url = "https://raw.githubusercontent.com/Ben9048/lua/main/opengate.lua" -- link
+local update_path = getWorkingDirectory() .. "/update.ini" -- link
+local update_url = "https://raw.githubusercontent.com/Ben9048/lua/main/update.ini?token=GHSAT0AAAAAACDSWBWIRKG5PFAUC7CSIWCGZFF4CRA"
+
 
 function main()
-	repeat wait(0) until isSampAvailable()
-	sampAddChatMessage("{ff7300}[Ben_Puls]: {ffffff}Fast open gate", -1)
-	wait(2000)
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
     sampRegisterChatCommand("update", cmd_update)
-	downloadUrlToFile(update_url, update_path, function(id, status) 
+
+    __, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    nick = sampGetPlayerNickname(id)
+
+
+
+    downloadUrlToFile(update_url, update_path, function(id, status)
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
            if tonumber(updateIni.info.vers) > script_vers then
-            sampAddChatMessage("{ff7300}[Ben_Puls]: {ffffff}Есть обновление! Версия: {808080}" .. updateIni.info.vers_text, -1)
-            update_state = true
+                sampAddChatMessage('{ffffff}Есть обновление! Версия: ' .. updateIni.info.vers_text, -1)
+                update_state = true
            end
            os.remove(update_path)
         end
+
     end)
 
 
+    while true do
+        wait(0)
+        if update_state then
+            downloadUrlToFile(script_url, script_path, function(id, status)
+                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+                    sampAddChatMessage('{ffffff}Скрипт успешно обновлён!', -1)
+                    thisScript():reload()
+              
+                end
+        
+            end)
+            break
+        end
+        end
 
-	
-		while true do
-	  wait(0)
+    end
 
-                if update_state then
-                    downloadUrlToFile(scirpt_url, script_path, function(id, status) 
-                        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    
-                                sampAddChatMessage("{ff7300}[Ben_Puls]: {ffffff}Скрипт успешно обновлён!", -1)
-                                thisScript():reload()
-                                                    
-                        end
-                    end)
-                    break
-                 end
-
-       
-end
 
 function cmd_update(arg)
-	sampShowDialog(1000, "Заголовок", "Описание\n v2.0", "Закрыть", "", 0)
-
-
-end
-
-if not sampIsChatInputActive() then
-    if isKeyJustPressed(VK_H) then
-          sampSendChat("/opengate")
-    end
-end
-
+    sampShowDialog(1000, "Заголовок v1.2", "Текст", "Закрыть", "", 0)
 end
