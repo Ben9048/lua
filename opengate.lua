77465820114d386f55c73312b8703e5f726d105c
FunBot <61308126+Ben9048@users.noreply.github.com>
2023-07-06 06:47:16 +0500

    Add files via upload

--- opengate.lua
+++ opengate.lua
@@ -1,68 +1,38 @@
-script_name("Model")
+require "lib.moonloader"
 script_author("Ben_Puls")
-script_description("Model")
-
-require 'lib.moonloader'
-local dlstatus = require('moonloader').download_status
-local inicfg = require 'inicfg'
-local keys = require 'vkeys'
-local imgui = require 'imgui'
-local encoding = require 'encoding'
-encoding.default = 'CP1251'
-u8 = encoding.UTF8
-
-update_state = false
-
-local script_vers = 1
-local script_vers_text = "1.00"
-local script_path = thisScript().path
-local script_url = "https://raw.githubusercontent.com/Ben9048/lua/main/opengate.lua" -- link
-local update_path = getWorkingDirectory() .. "/update.ini" -- link
-local update_url = "https://raw.githubusercontent.com/Ben9048/lua/main/update.ini?token=GHSAT0AAAAAACDSWBWIRKG5PFAUC7CSIWCGZFF4CRA"
+script_description("fast open gate")
+script_version("1.00")
+local keys = require "vkeys"
+local sampev = require 'lib.samp.events'
+local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
+local autoupdate_loaded = false
+local Update = nil
+if enable_autoupdate then
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    if updater_loaded then
+        autoupdate_loaded, Update = pcall(Updater)
+        if autoupdate_loaded then
+            Update.json_url = "https://raw.githubusercontent.com/Ben9048/lua/main/update.json" .. tostring(os.clock())
+            Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
+            Update.url = "https://github.com/Ben9048/lua/"
+        end
+    end
+end
 
 
 function main()
-    if not isSampLoaded() or not isSampfuncsLoaded() then return end
-    while not isSampAvailable() do wait(100) end
-
-    sampRegisterChatCommand("update", cmd_update)
-
-    __, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
-    nick = sampGetPlayerNickname(id)
-
-
-
-    downloadUrlToFile(update_url, update_path, function(id, status)
-        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-            updateIni = inicfg.load(nil, update_path)
-           if tonumber(updateIni.info.vers) > script_vers then
-                sampAddChatMessage('{ffffff}Есть обновление! Версия: ' .. updateIni.info.vers_text, -1)
-                update_state = true
-           end
-           os.remove(update_path)
-        end
-
-    end)
-
-
-    while true do
-        wait(0)
-        if update_state then
-            downloadUrlToFile(script_url, script_path, function(id, status)
-                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('{ffffff}Скрипт успешно обновлён!', -1)
-                    thisScript():reload()
-              
-                end
-        
-            end)
-            break
-        end
-        end
-
+	repeat wait(0) until isSampAvailable()
+	if autoupdate_loaded and enable_autoupdate and Update then
+        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
     end
-
-
-function cmd_update(arg)
-    sampShowDialog(1000, "Заголовок v1.2", "Текст", "Закрыть", "", 0)
+	sampAddChatMessage("{ff7300}[Ben_Puls]: {ffffff}Fast open gate", -1)
+	wait(2000)
+		while true do
+	  wait(0)
+          if not sampIsChatInputActive() then
+	  if isKeyJustPressed(VK_H) then
+			sampSendChat("/opengate")
+	  end
+end
+end
 end
